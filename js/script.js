c35a4f085410d0ae61208a7668e749873fd0f82c
Roy Are Øvrebotten <69842808+raovrebo@users.noreply.github.com>
2021-01-06 18:51:37 +0100

    Finished the Lesson Tast 2 Questions

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// Q1
+var integer = 6;
+var decimal = 3.5;
+// console.log(integer);
+// console.log(decimal);
+// Q2
+var workingDays = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
+//console.log(workingDays);
+// Q3
+var addition = 3 + 2;
+var subtraction = 10 - 4;
+var multiplication = 6 * 12;
+var division = 18 / 3;
+// console.log(addition);
+// console.log(subtraction);
+// console.log(multiplication);
+// console.log(division);
+// Q4
+var onSpecial = true;
+// console.log(onSpecial);
+// Q5
+var sentence = "The frogs are angry";
+//console.log(sentence.length);
+// Q6
+var timeElapsed;
+var totalPrice;
+// Q7
+var number1 = "53";
+var number2 = "44.6";
+convertNumber1 = parseInt(number1);
+convertNumber2 = parseFloat(number2);
+// console.log(convertNumber1)
+// console.log(convertNumber2)
+// Q8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeleft;
+
+var typeOfPet = typeof pet;
+// console.log(typeOfPet);
+
+var typeOfAge = typeof age;
+// console.log(typeOfAge);
+
+var typeOfDiscountApplied = typeof discountApplied;
+// console.log(typeOfDiscountApplied);
+
+var typeTimeLeft = typeof timeleft;
+// console.log(typeOfTimeLeft);
+// Q9
+var headerText = "Updating heading";
+
+var heading = document.querySelector("h1");
+heading.innerHTML = headerText;
+// Q10
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
